5add7b29b68f667b6f447088776c4ce61fcff3f0
admin <admin@peng.cn>
2022-11-10 16:58:35 +0800

    Initial commit

---
+++ reggie_take_out/src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
@@ -0,0 +1,134 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.CustomException;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
+import com.itheima.reggie.entity.SetmealDish;
+import com.itheima.reggie.mapper.DishMapper;
+import com.itheima.reggie.service.DishFlavorService;
+import com.itheima.reggie.service.DishService;
+import com.itheima.reggie.service.SetmealDishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+
+    @Autowired
+    private DishFlavorService dishFlavorService;
+
+    @Autowired
+    private SetmealDishService  setmealDishService;
+
+
+    /**
+     * 新增菜品，同时保存对应的口味数据
+     *
+     * @param dishDto
+     */
+    @Transactional
+    public void saveWithFlavor(DishDto dishDto) {
+        //保存菜品的基本信息到菜品表dish
+        this.save(dishDto);
+
+        Long dishId = dishDto.getId(); //菜品id
+
+        //菜品口味
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors = flavors.stream().map((item) -> {
+            item.setDishId(dishId);
+            return item;
+        }).collect(Collectors.toList());
+
+        //保存菜品口味数据到菜品口味表dish_flavor
+        dishFlavorService.saveBatch(flavors);
+
+    }
+
+
+    /**
+     * 根据id查询菜品信息和对应的口味信息
+     *
+     * @param id
+     * @return
+     */
+    @Transactional
+    public DishDto getByIdWithFlavor(Long id) {
+        //查询菜品基本信息，从dish表查询
+        Dish dish = this.getById(id);
+
+        DishDto dishDto = new DishDto();
+        BeanUtils.copyProperties(dish, dishDto);
+
+        //查询当前菜品对应的口味信息，从dish_flavor表查询
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId, dish.getId());
+        List<DishFlavor> flavors = dishFlavorService.list(queryWrapper);
+        dishDto.setFlavors(flavors);
+
+        return dishDto;
+    }
+
+
+    /**
+     * 更新菜品信息，同时更新对应的口味信息
+     * @param dishDto
+     */
+    @Transactional
+    public void updateWithFlavor(DishDto dishDto) {
+        //更新dish表基本信息
+        this.updateById(dishDto);
+
+        //清理当前菜品对应口味数据---dish_flavor表的delete操作
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId,dishDto.getId());
+
+        dishFlavorService.remove(queryWrapper);
+
+        //添加当前提交过来的口味数据---dish_flavor表的数据insert操作
+        List<DishFlavor> flavors = dishDto.getFlavors();
+
+        flavors = flavors.stream().map((item) -> {
+            item.setDishId(dishDto.getId());
+            return item;
+        }).collect(Collectors.toList());
+
+        dishFlavorService.saveBatch(flavors);
+    }
+
+
+    /**
+     * 删除菜品，同时删除菜品对应的口味
+     * @param ids
+     */
+    @Transactional
+    public void deleteWithFlavor(Long[] ids) {
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.in(Dish::getId,ids);
+        queryWrapper.eq(Dish::getStatus,1);
+
+        int count = this.count(queryWrapper);
+        if(count > 0){
+            throw new CustomException("当前菜品处于售卖中，不能删除");
+        }
+
+        this.removeByIds(Arrays.asList(ids));
+
+        LambdaQueryWrapper<DishFlavor> queryWrapper1 = new LambdaQueryWrapper<>();
+        queryWrapper1.in(DishFlavor::getDishId,ids);
+
+        dishFlavorService.remove(queryWrapper1);
+    }
+}
---
+++ reggie_take_out/src/main/java/com/itheima/reggie/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,128 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.CustomException;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.entity.SetmealDish;
+import com.itheima.reggie.mapper.SetmealMapper;
+import com.itheima.reggie.service.SetmealDishService;
+import com.itheima.reggie.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
+
+    @Autowired
+    private SetmealDishService setmealDishService;
+
+    /**
+     * 新增套餐，同时需要保存套餐和菜品的关联关系
+     * @param setmealDto
+     */
+    @Transactional
+    public void saveWithDish(SetmealDto setmealDto) {
+        //保存套餐的基本信息，操作setmeal，执行insert操作
+        this.save(setmealDto);
+
+        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
+        setmealDishes.stream().map((item) -> {
+            item.setSetmealId(setmealDto.getId());
+            return item;
+        }).collect(Collectors.toList());
+
+        //保存套餐和菜品的关联信息，操作setmeal_dish，执行insert操作
+        setmealDishService.saveBatch(setmealDishes);
+
+    }
+
+
+    /**
+     * 删除套餐，同时需要删除套餐和菜品的关联数据
+     * @param ids
+     */
+    @Transactional
+    public void removeWithDish(List<Long> ids) {
+        //查询套餐状态，确定是否可以删除
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.in(Setmeal::getId,ids);
+        queryWrapper.eq(Setmeal::getStatus,1);
+
+        //如果不能删除，抛出一个业务异常
+        int count = this.count(queryWrapper);
+        if(count > 0){
+            throw new CustomException("当前套餐处于售卖中，不能删除");
+        }
+
+        //如果可以删除，先删除套餐表中的数据
+        this.removeByIds(ids);
+
+        //删除关系表中的数据
+        LambdaQueryWrapper<SetmealDish> queryWrapper1 = new LambdaQueryWrapper<>();
+        queryWrapper1.in(SetmealDish::getSetmealId,ids);
+
+        setmealDishService.remove(queryWrapper1);
+    }
+
+
+    /**
+     * 根据id查询套餐信息和对应菜品信息
+     * @param id
+     */
+    @Transactional
+    public SetmealDto getByIdWithDish(Long id) {
+        //查询套餐基本信息，从setmeal表查询
+        Setmeal setmeal = this.getById(id);
+
+        SetmealDto setmealDto = new SetmealDto();
+        BeanUtils.copyProperties(setmeal, setmealDto);
+
+        //查询当前套餐对应的菜品信息，从setmeal_dish表查询
+        LambdaQueryWrapper<SetmealDish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(SetmealDish::getSetmealId, setmeal.getId());
+        List<SetmealDish> setmealDishes = setmealDishService.list(queryWrapper);
+        setmealDto.setSetmealDishes(setmealDishes);
+
+        return setmealDto;
+    }
+
+
+    /**
+     * 更新套餐信息，同时更新对应的菜品信息
+     * @param setmealDto
+     */
+    @Transactional
+    public void updateWithDish(SetmealDto setmealDto) {
+        //更新setmeal表基本信息
+        this.updateById(setmealDto);
+
+        //清理当前套餐对应菜品数据---setmeal_dish表的delete操作
+        LambdaQueryWrapper<SetmealDish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(SetmealDish::getSetmealId,setmealDto.getId());
+
+        setmealDishService.remove(queryWrapper);
+
+        //添加当前提交过来的菜品数据---setmeal_dish表的数据insert操作
+        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
+
+        setmealDishes = setmealDishes.stream().map((item) -> {
+            item.setSetmealId(setmealDto.getId());
+            return item;
+        }).collect(Collectors.toList());
+
+        setmealDishService.saveBatch(setmealDishes);
+    }
+}
